35da65a42cbbb3145eafd130bc8208035db1136d
zhucherofficial <zcycycycy@gmail.com>
2026-09-03 16:49:13 +0800

    Fix ESLint config for Vite
    Add Node globals to the ESLint configuration for `vite.config.js` so Vite config files lint correctly without false positives from Node APIs.

--- eslint.config.js
+++ eslint.config.js
@@ -18,4 +18,10 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    files: ['vite.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ])
